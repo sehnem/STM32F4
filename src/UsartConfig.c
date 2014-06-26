89c7710ac6c51e78fbb0279f9e8c97f409477701
Josué M. Sehnem <josue@sehnem.com>
2014-06-26 09:19:12 -0300

    usart

--- src/UsartConfig.c
+++ src/UsartConfig.c
@@ -1,1 +1,16 @@
 #include <UsartConfig.h>
+
+void Usart1Init()
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+
+
+
+	USART_InitStructure.BaudRate = 9600;
+	USART_InitStructure.WordLength = USART_WORDLENGTH_8B;
+	USART_InitStructure.StopBits = USART_STOPBITS_1;
+	USART_InitStructure.Parity = USART_PARITY_NONE;
+	USART_InitStructure.Mode = USART_MODE_TX_RX;
+
+}
